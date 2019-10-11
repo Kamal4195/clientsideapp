c06752c32e9b6f1496f21570657d5ffa9dc70286
Kamal Reddy <kamalreddy4195gmail.com>
2019-10-11 12:48:14 -0500

    initial commit

--- components/averager/averager.js
+++ components/averager/averager.js
@@ -1,7 +1,7 @@
-const avg = (x, y) => { return x / y }
+const add = (x, y) => { return x / y }
 
-const averager = new Vue({
-  el: '#averager',
+const adder = new Vue({
+  el: '#adder',
   data: {
     guest: 'kamal',
     firstNumber: 5,
@@ -11,7 +11,7 @@ const averager = new Vue({
     result: function () {
       const i = parseInt(this.firstNumber)
       const j = parseInt(this.secondNumber)
-      return `${this.guest}, displaying workload: ${avg(i, j)}.`
+      return `${this.guest}, displaying workload: ${add(i, j)}.`
     }
   }
 })
